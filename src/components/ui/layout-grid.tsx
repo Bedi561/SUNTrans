4c30a8dcd2e5942c5c48980249ad655f812121be
Bedi561 <110516777+Bedi561@users.noreply.github.com>
2025-06-30 16:19:21 +0530

    all2

--- src/components/ui/layout-grid.tsx
+++ src/components/ui/layout-grid.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { motion } from "motion/react";
 import { cn } from "@/lib/utils";
+import { JSX } from "react/jsx-runtime";
 
 type Card = {
   id: number;
