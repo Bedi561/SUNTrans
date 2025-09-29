99cf317bdddcc129be348be9846ecd9e7b3af8b6
Bedi561 <110516777+Bedi561@users.noreply.github.com>
2025-09-30 03:05:25 +0530

    allin1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import About from "@/components/sections/About";
 import Services from "@/components/sections/Services";
 // import WhyChooseUs from "@/components/sections/WhyChoose";
 // import Gallery from "@/components/sections/Gallery";
-import StatsBanner from "@/components/sections/StatsBanner";
+// import StatsBanner from "@/components/sections/StatsBanner";
 // import Testimonials from "@/components/sections/TestimonialSection";
 import ClientShowcase from "@/components/sections/ClientShowcase";
 import Footer from "@/components/sections/Footer";
